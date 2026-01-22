2807a5cdfca12ca16826e6cdaa704808111cda33
Kojima22 <w86f9x@gmail.com>
2026-01-21 20:50:07 -0800

    Initial Commit

---
+++ imgTools/imgToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  imgToolsApp.swift
+//  imgTools
+//
+//  Created by zzc on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct imgToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
